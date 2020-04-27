938e0f2a7eb395e89eeb506010f7ca80c2ee6ef8
Romina Gomez <romina@starred.com>
2020-04-27 21:51:08 +0200

    About, skills, work

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,10 @@
 $('.menu > ul, li').off('click').on('click', function(e){
   e.stopPropagation()
   $('#const').addClass('constHide')
-  $('.menu-container').addClass('menuMove')
+  $('h1 > .green').addClass('menuMove')
   $('h1').addClass('h1Shrink')
   $('.menu').addClass('menuShrink')
-  $('li').each( (i,el) => {
-    console.log(el.id)
-    console.log(this.id)
+  $('.menu-li').each( (i,el) => {
     if (el.id === this.id){
       $(this).addClass('white')
     } else {
@@ -14,4 +12,14 @@ $('.menu > ul, li').off('click').on('click', function(e){
       el.classList.remove('white')
     }
   })
+  $('.pages > div').removeClass('display-content').css('display', 'none');
+  if(this.id == 'about'){
+    $('#about-content').addClass('display-content').css('display', 'block');
+  }
+  if(this.id == 'skills'){
+    $('#skills-content').addClass('display-content').css('display', 'flex');
+  }
+  if(this.id == 'work'){
+    $('#work-content').addClass('display-content').css('display', 'flex');
+  }
 })
