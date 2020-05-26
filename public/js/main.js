e20511028bb642022ec7b907f1c54ebb4efd1624
Romina Gomez <romina@starred.com>
2020-05-26 15:41:27 +0200

    Office work

--- public/js/main.js
+++ public/js/main.js
@@ -22,4 +22,40 @@ $('.menu > ul, li').off('click').on('click', function(e){
   if(this.id == 'work'){
     $('#work-content').addClass('display-content').css('display', 'flex');
   }
+  if(this.id == 'contact'){
+    $('#contact-content').addClass('display-content').css('display', 'flex');
+  }
 })
+
+$('#next').on('click', function(e){
+  e.stopPropagation()
+  $('#experience-page1').removeClass('display-content').css('display', 'flex').css('display', 'none');
+  $('#experience-page2').addClass('display-content').css('display', 'flex');
+  $('#previous').css('display', 'block');
+  $('#next').css('display', 'none');
+
+})
+$('#previous').on('click', function(e){
+  e.stopPropagation()
+  $('#experience-page2').removeClass('display-content').css('display', 'none');
+  $('#experience-page1').addClass('display-content').css('display', 'flex');
+  $('#previous').css('display', 'none');
+  $('#next').css('display', 'block');
+})
+
+$('#present').on('click', function(e){
+  e.stopPropagation()
+  $(this).removeClass('grey').addClass('green');
+  $("#past").removeClass('green').addClass('grey');
+  $(".present-section").addClass('display-content').css('display', 'block');
+  $('.past-section').removeClass('display-content').css('display', 'none');
+})
+
+$('#past').on('click', function(e){
+  e.stopPropagation()
+  $(this).removeClass('grey').addClass('green');
+  $("#present").removeClass('green').addClass('grey');
+  $(".past-section").addClass('display-content').css('display', 'block');
+  $('.present-section').removeClass('display-content').css('display', 'none');
+})
+
